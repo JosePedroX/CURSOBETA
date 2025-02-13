24eb4bb591c48b97ef436ad8c8025887a5208ce1
jose PedroX <xsjosepedro@gmail.com>
2025-02-13 11:22:26 -0300

    curso1.0.3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 const comprarBotao = document.getElementById('comprar');
 comprarBotao.addEventListener('click', () => {
-    window.location.href=''
+    window.location.href='https://pay.kiwify.com.br/8HfG4rr'
 });
